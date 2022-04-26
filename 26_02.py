b0fc3348424204edc169213155c8fbab076b3ac1
Camila Hinojosa <chinojosa@miuandes.cl>
2022-04-26 09:01:37 -0400

    definir estructura base del reporte

---
+++ 26_02.py
@@ -0,0 +1,5 @@
+""""
+git permite monitorear las modificaciones de las carpetas, pero se basa en las fotos que voy haciendo.
+yo tengo que decirle que guarde los cambios. voy sacando las fotos que yo quiero a mi codigo
+
+"""
---
+++ reporte.md
@@ -0,0 +1,5 @@
+#introduccion
+
+#desarrollo
+
+#conclusion
